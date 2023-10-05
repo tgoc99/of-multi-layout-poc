2e92a254d1dea868e2a5353db6c3507e1b4253f1
George Allakhverdyan <george.allakhverdyan@openfin.co>
2023-10-05 11:00:24 -0500

    fix: add close-tab logic

--- app/components/LayoutContainer.tsx
+++ app/components/LayoutContainer.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Box, Tab, Tabs } from "@mui/material";
+import { Box, IconButton, Tab, Tabs } from "@mui/material";
 import OpenFin, { fin } from "@openfin/core";
 import React, { useEffect } from "react";
 import { Layout } from "./Layout";
 import { LayoutState, styles } from "./types";
+import { Close } from "@mui/icons-material";
 
 /**
  * The div container providing a Tabbed interface of layouts.
@@ -33,7 +34,7 @@ export const LayoutContainer = () => {
 
       // Not necessary for this example - to be called by OpenFin API internals
       // (i.e. replaceLayout API or to show the corresponding layout when view.focus() is called)
-      public showLayout = async (id: OpenFin.LayoutIdentity) => {
+      showLayout = async (id: OpenFin.LayoutIdentity) => {
         if (id.layoutName) {
           const newIndex = layoutState.findIndex(
             (x) => x.layoutName === id.layoutName
@@ -56,6 +57,25 @@ export const LayoutContainer = () => {
     setCurrentActiveTab(Math.min(index, layoutState.length - 1));
   };
 
+  const handleTabClose = (
+    ev: React.SyntheticEvent,
+    layoutName: string,
+    index: number
+  ) => {
+    // prevent tab change logic
+    ev.stopPropagation();
+
+    // change the active tab
+    setCurrentActiveTab(
+      currentActiveTab < index
+        ? currentActiveTab
+        : Math.max(0, currentActiveTab - 1)
+    );
+
+    // set state to reactfully destroy the layout via fin.Platform.Layout.destroy()
+    setLayoutState(layoutState.filter((l) => l.layoutName !== layoutName));
+  };
+
   return (
     <div id="layout-container">
       <Box sx={{ width: "100%", height: "100%" }}>
@@ -67,8 +87,21 @@ export const LayoutContainer = () => {
             sx={styles.tabContainer}
             TabIndicatorProps={{ sx: styles.tabIndicator }}
           >
-            {layoutState.map(({ layoutName }) => (
-              <Tab sx={styles.tab} key={layoutName} label={layoutName} />
+            {layoutState.map(({ layoutName }, i) => (
+              <Tab
+                sx={styles.tab}
+                key={layoutName}
+                label={layoutName}
+                iconPosition="end"
+                icon={
+                  <IconButton sx={styles.iconButton}>
+                    <Close
+                      sx={styles.smallIcon}
+                      onClick={(ev) => handleTabClose(ev, layoutName, i)}
+                    />
+                  </IconButton>
+                }
+              />
             ))}
           </Tabs>
         </Box>
